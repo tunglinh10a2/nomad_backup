8d6d73c86a1086000d0229f577025a996129d01b
Đậu Tùng Linh <linh.dt2@citigo.id>
2022-10-12 15:30:08 +0700

    Add docker to vagrantfile

--- install_nomad.sh
+++ install_nomad.sh
@@ -2,7 +2,7 @@ curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
 
 sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
 
-sudo apt-get update && sudo apt-get install nomad
+sudo apt-get update && sudo apt-get install -y nomad
 
 sudo systemctl stop nomad
 
@@ -10,3 +10,20 @@ sudo systemctl enable nomad
 
 sudo systemctl start nomad
 
+sudo apt-get remove docker docker-engine docker.io containerd runc
+
+sudo apt-get update
+
+sudo apt-get install -y ca-certificates curl gnupg lsb-release
+
+sudo mkdir -p /etc/apt/keyrings
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+sudo service docker start
